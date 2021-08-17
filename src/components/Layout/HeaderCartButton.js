2b88f8239733cc9cdebca7720815b44074118c05
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2021-08-17 22:59:16 +0300

    final

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    setBtnHighlighted(true);
+  }, []);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-          3
-      </span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
